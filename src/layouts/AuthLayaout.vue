04621168729c73071f668e3d790465eaa2ae8f16
JulioSalazar12 <jsalazarzapata2@gmail.com>
2023-06-05 09:16:10 -0500

    feat: style new added in login view

--- src/layouts/AuthLayaout.vue
+++ src/layouts/AuthLayaout.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="authLayout">
+  <div class="authLayout" ref='vantaRef'>
     <slot></slot>
   </div>
 </template>
   
 <script>
+import CELLS from 'vanta/dist/vanta.waves.min'
 
   export default {
     name: 'AuthLayaout',
@@ -15,10 +16,15 @@
     computed:{
     },
     mounted() {
-      
+      this.vantaEffect = CELLS({
+        el: this.$refs.vantaRef,
+        color: 0x051252,
+      })
     },
-    methods:{
-      
+    beforeDestroy() {
+      if (this.vantaEffect) {
+        this.vantaEffect.destroy()
+      }
     }
 
   }
