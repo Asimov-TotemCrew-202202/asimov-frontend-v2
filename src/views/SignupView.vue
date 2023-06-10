6e4aa6f904bca3888f06ffbad3b04e9ce42a3c08
Heber Cordova <u201924464@upc.edu.pe>
2023-06-10 11:34:21 -0500

    feat: added singup with api

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -9,6 +9,8 @@
               <v-form ref="form" lazy-validation>
                 <v-text-field class="my-3"
                     dense
+                    v-model="user.first_name"
+                    :rules="firstNameRules"
                     hide-details="auto"
                     label="Nombre y Apellidos"
                     placeholder="Juan Sanchez"
@@ -26,10 +28,14 @@
                 
                 <v-text-field class="my-3"
                     dense
+                    @blur="createUsername"
+                    :rules="emailRules"
                     hide-details="auto"
                     label="E-mail"
+                    outlined
                     placeholder="ejemplo@mail.com"
-                    required outlined
+                    required 
+                    v-model="user.email"
                 >
                   <template v-slot:append>
                     <v-tooltip bottom>
@@ -42,6 +48,7 @@
                 </v-text-field>
                 <v-text-field class="my-3"
                     disabled
+                    v-model="user.username"
                     dense
                     hide-details="auto"
                     label="Nombre de usuario"
@@ -59,6 +66,8 @@
                 </v-text-field>
                 <v-text-field class="my-3"
                     dense
+                    v-model="user.phone"
+                    :rules="phoneRules"
                     type="number"
                     hide-details="auto"
                     label="Celular"
@@ -77,6 +86,8 @@
                 
                 <v-text-field class="my-3"
                     dense
+                    v-model="user.last_name"
+                    :rules="lastNameRules"
                     hide-details="auto"
                     label="Institución Educativa"
                     placeholder="Colegio I.E."
@@ -94,6 +105,8 @@
                 <v-text-field class="mt-3 mb-4"
                     dense
                     hide-details="auto"
+                    :rules="passwordRules"
+                    v-model="user.password"
                     label="Contraseña"
                     placeholder="*******"
                     type="password"
@@ -113,7 +126,7 @@
         <v-card-actions>
           <v-row>
             <v-col cols="12" class="pb-0">
-              <v-btn block color="#0b2ac4" dark elevation="0"  class="mb-2">
+              <v-btn @click="register" block color="#0b2ac4" dark elevation="0"  class="mb-2">
                 REGISTRAR
               </v-btn>
               <v-btn block text @click="pushToHome">
@@ -125,23 +138,117 @@
         </v-card-actions>
       </v-card>
     </v-hover>
+    <v-snackbar
+      :color="successful? 'green' : 'red'"
+      v-model="showResult"
+    >
+      {{ message }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="white"
+          text
+          v-bind="attrs"
+          @click="showResult = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
   </div>
 </template>
 
 <script>
   // import HelloWorld from '../components/HelloWorld'
   import router from "@/router";
+  import UserRegister from '../models/userRegister';
 
 
   export default {
     name: 'LoginView',
+    data() {
+      return {
+        user: new UserRegister('', '', '', '', '', '', []),
+        showResult: false,
+        message: '',
+        successful: false,
+        firstNameRules: [
+          value => {
+            if (value) return true
 
+            return 'Full name is required.'
+          }
+        ],
+        lastNameRules: [
+          value => {
+            if (value) return true
+
+            return 'Educational institution is required.'
+          }
+        ],
+        passwordRules: [
+          value => {
+            if (value) return true
+
+            return 'Password is required.'
+          }
+        ],
+        emailRules: [
+          value => {
+            if (value) return true
+
+            return 'Email is required.'
+          }
+        ],
+        phoneRules: [
+          value => {
+            if (value) return true
+
+            return 'Phone is required.'
+          }
+        ],
+      }
+    },
     components: {
       // HelloWorld,
     },
     methods:{
       pushToHome() {
         router.push('/');
+      },
+      createUsername() {
+        const username = this.user.email.substring(0, this.user.email.indexOf('@'));
+
+        this.user.username = username;
+      },
+      async register() {
+
+        const { valid } = await this.$refs.form.validate();
+
+        if (!valid) {
+          this.user.role.push('principal');
+
+          await this.$store.dispatch('auth/register', this.user).then(
+              async data => {
+                this.message = data.message;
+                this.successful = true;
+                this.showResult = true;
+
+                await this.$refs.form.reset();
+
+                setTimeout(() => {
+                  this.$router.push('/').catch(()=>{});
+                }, 1000);
+                
+              },
+              error => {
+                this.message = error.response.data.message;
+                this.successful = false;
+
+                this.showResult = true;
+              }
+            );
+        }
       }
     }
   }
